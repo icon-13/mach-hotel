f4a3d70f20c03cdc46d03cc2410944b17afdfce6
icon-13 <jimmymjema@gmail.com>
2026-01-25 09:29:34 +0300

    Fix TrustProxies headers for Render

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * Trust all proxies (Render sits in front of your container).
+     */
+    protected $proxies = '*';
+
+    /**
+     * Trust these forwarded headers (proto/host/port/for).
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO;
+}
